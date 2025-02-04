075a33a7d1d801c8232e35b7c7d1e41de1c84aea
Nelson Fonkoua <tambapps@gmail.com>
2025-02-04 21:31:10 +0100

    better.

--- app/lib/ui/screen/about/about_screen.dart
+++ app/lib/ui/screen/about/about_screen.dart
@@ -49,6 +49,9 @@ class _AboutScreenState extends AbstractScreenState<AboutScreen> {
             Align(alignment: Alignment.topCenter, child: Text("About me", style: theme.textTheme.titleLarge,),),
             Padding(padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0), child: _aboutMeText(),),
             const SizedBox(height: 64.0,),
+            Align(alignment: Alignment.topCenter, child: Text("About the app", style: theme.textTheme.titleLarge,),),
+            Padding(padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0), child: _aboutTheAppText(),),
+            const SizedBox(height: 64.0,),
             Align(alignment: Alignment.topCenter, child: Text("Credits", style: theme.textTheme.titleLarge,),),
             Padding(padding: EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0), child: _creditsText(),),
         ],),));
@@ -75,38 +78,77 @@ class _AboutScreenState extends AbstractScreenState<AboutScreen> {
             style: linkTextStyle,
             recognizer: TapGestureRecognizer()..onTap = () => openLink("https://github.com/tambapps/"),
           ),
-          TextSpan(
-            text: "\n",
-          ),
-          TextSpan(
-            text: "You can check the source code of this app ",
-          ),
-          TextSpan(
-            text: "here",
-            style: linkTextStyle,
-            recognizer: TapGestureRecognizer()..onTap = () => openLink("https://github.com/tambapps/pokemon-teamlytics/"),
-          )
         ],
       ),
       textAlign: TextAlign.justify,
     );
   }
-  Widget _creditsText() {
-    return Text.rich(
-      TextSpan(
-        text: "",
-        children: [
+
+
+  Widget _aboutTheAppText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("I built this app because I wanted one single UI in which I could keep track of how I use a team and take notes to improve myself and know how to handle different match-ups.",
+          textAlign: TextAlign.justify,),
+        Text.rich(
           TextSpan(
-            text: "jormxdos",
-            style: linkTextStyle,
-            recognizer: TapGestureRecognizer()..onTap = () => openLink("https://www.deviantart.com/jormxdos"),
+            text: "",
+            children: [
+              TextSpan(
+                text: "This app ",
+              ),
+              TextSpan(
+                  text: "collects no data",
+                  style: TextStyle(fontWeight: FontWeight.bold)
+              ),
+              TextSpan(
+                text: ".",
+              ),
+              TextSpan(
+                text: " All data is saved on your local storage (of your web browser or mobile phone).",
+              ),
+              TextSpan(
+                text: " You can check the source code of this app ",
+              ),
+              TextSpan(
+                text: "here",
+                style: linkTextStyle,
+                recognizer: TapGestureRecognizer()..onTap = () => openLink("https://github.com/tambapps/pokemon-teamlytics/"),
+              ),
+              TextSpan(
+                text: ".",
+              )
+            ],
           ),
-          TextSpan(
-            text: " to have designed the tera type logos",
-          ),
-        ],
-      ),
-      textAlign: TextAlign.justify,
+          textAlign: TextAlign.justify,
+        )
+      ],
     );
+  }
+
+  Widget _creditsText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text.rich(
+          TextSpan(
+            text: "",
+            children: [
+              TextSpan(
+                text: "jormxdos",
+                style: linkTextStyle,
+                recognizer: TapGestureRecognizer()..onTap = () => openLink("https://www.deviantart.com/jormxdos"),
+              ),
+              TextSpan(
+                text: " to have designed the tera type logos.",
+              ),
+            ],
+          ),
+          textAlign: TextAlign.justify,
+        ),
+        const SizedBox(height: 8.0,),
+        Text("Shoutout to me, to have developed the whole app by myself from scratch.", textAlign: TextAlign.justify,)
+      ],);
   }
 }
